7e59bafc6e00aab7421dea2e2bf6a8080b1240c0
1seanob1 <m155202>
2015-04-20 20:39:10 -0400

    a

--- getter.py
+++ getter.py
@@ -4,30 +4,42 @@ from os import listdir
 from os.path import isfile, join
 glob=0
 def getFiles(dirName):
-        itr=0
-#http://stackoverflow.com/questions/3207219/how-to-list-all-files-of-a-directory-in-python
-	files=listdir(dirName)
-	files2=[]
-	for f in files:
-		if(isfile(join(dirName,f))):
-                        itr=jRead(dirName,f,itr)
+        try:
+                itr=0
+                #http://stackoverflow.com/questions/3207219/how-to-list-all-files-of-a-directory-in-python
+                files=listdir(dirName)
+                files2=[]
+                for f in files:
+                        if(isfile(join(dirName,f))):
+                                itr=jRead(dirName,f,itr)
+        except:
+                pass
 def write(text,itr):
-        f=open("./data/"+str(itr),"w")
-        f.write(text)
-        f.close()
+        try:
+                f=open("./data/"+str(itr),"w")
+                f.write(text)
+                f.close()
+        except:
+                pass
         #world
 def get(html,itr):
-        #http://stackoverflow.com/questions/645312/what-is-the-quickest-way-to-http-get-in-python
-        write(urllib2.urlopen("http://"+html).read(),itr)
+        try:
+                #http://stackoverflow.com/questions/645312/what-is-the-quickest-way-to-http-get-in-python
+                write(urllib2.urlopen("http://"+html).read(),itr)
+        except:
+                pass
 def jRead(dirName,fname,itr):
-#http://stackoverflow.com/questions/2835559/parsing-values-from-a-json-file-in-python
-        print dirName
-        with open(join(dirName+"/",fname)) as json_data:
-                data=json.load(json_data)
-        for x in data:
-                get(fname+x["link"][0],itr)
-                itr+=1
-        return itr
+        try:
+                #http://stackoverflow.com/questions/2835559/parsing-values-from-a-json-file-in-python
+                print dirName
+                with open(join(dirName+"/",fname)) as json_data:
+                        data=json.load(json_data)
+                        for x in data:
+                                get(fname+x["link"][0],itr)
+                                itr+=1
+                return itr
+        except:
+                pass
 def main():
         glob=0
         getFiles("./data")
