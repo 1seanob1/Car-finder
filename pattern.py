ad431ce369dd6db4d02b6bc2f5d892edf41ab93b
1seanob1 <m155202>
2015-04-22 19:25:09 -0400

    suck it stanko

--- pattern.py
+++ pattern.py
@@ -2,34 +2,42 @@ import nltk
 import tokenizer
 import re
 
-folder = 'data2/'
+folder = 'data/'
 goldStandard=dict()
 modelCount_wMatch = dict()
 modelCount = dict()
 gold = list()
 def countPattern():
+        #totalMatch is the total number of times any pattern is found in the data
+        totalMatch=0
+        #totalModel is the total number of times a particular model/key is found in the text, regardless of patterns
+        totalModel=0
 	global goldStandard, gold, modelCount
 	#loop
 	print "Tokenize"
-	for ii in range (0,376):
+#	for ii in range (0,376):
 		#tokenize
-		tokenizer.tokenize(folder+str(ii))		
+#		tokenizer.tokenize(folder+str(ii))		
 		#toppatterns
-	goldStandard = tokenizer.topPatterns()
+	goldStandard = tokenizer.topModelPatterns(list())
 	gold = list(goldStandard) 
 	
 	print "Got Gold"  
 	print gold   
-	for ii in range (0,376):
+	for ii in range (0,99):
 		
 		fo = open(folder+str(ii), "r")
 		lines = fo.read()
 		#find_match
 		m = re.findall(gold[0][0]+ ' ' + gold[0][1] + ' \w+ '+ gold[0][2] + ' '+ gold[0][3], lines)
-		
+
+                #Increment totalMatch by the number of times a pattern was found 
+		totalMatch+=len(m)
 		
 		#extract models
 		for element in m:
+                        
+                        totalMatch+=1
 			temp = element.split()
 			model = temp[2]
 			#count		
@@ -37,15 +45,14 @@ def countPattern():
 				modelCount_wMatch[model]+= 1
 			else:
 				modelCount_wMatch[model]= 1
-		
-		for model in modelCount_wMatch: 
-			#print model
-			found = re.findall(model, lines)
-			
-			print count
-		modelCount = count
-		
 		#nextfile
+        for model in modelCount_wMatch: 
+                #print model
+                found = re.findall(model, lines)
+                modelCount_wMatch[model]=tokenizer.MatchPmi(modelCount_wMatch[model],tokenizer.tokenLen(),totalMatch,len(found))
+        
+        print modelCount_wMatch
+		
 
 
 
